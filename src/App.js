51aed7bb47cda3cba68083cf1de4f0c37f5857f8
Feline Huhn <feline.huhn@gmx.de>
2023-11-06 14:17:04 +0100

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click Me!</Button>
+      <Button>Click me too!</Button>
+      <Button>Click also me!</Button>
+      <Button>Click the button</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
